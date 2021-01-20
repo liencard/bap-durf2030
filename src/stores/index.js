9499ad306fa3de19034af3c9d5d0664feefe733d
Sarah Van Den Berghe <sarah.van.den.berghe@student.howest.be>
2021-01-20 16:13:45 +0100

    removed comments

--- src/stores/index.js
+++ src/stores/index.js
@@ -7,15 +7,6 @@ import ProjectStore from './ProjectStore';
 class RootStore {
   constructor() {
     this.firebase = this.getFirebase();
-    // var firebaseConfig = {
-    //   apiKey: process.env.REACT_APP_apiKey,
-    //   authDomain: process.env.REACT_APP_authDomain,
-    //   projectId: process.env.REACT_APP_projectId,
-    //   storageBucket: process.env.REACT_APP_storageBucket,
-    //   messagingSenderId: process.env.REACT_APP_messagingSenderId,
-    //   appId: process.env.REACT_APP_appId,
-    // };
-
     this.userStore = new UserStore(this);
     this.projectStore = new ProjectStore(this);
     this.uiStore = new UiStore(this);
