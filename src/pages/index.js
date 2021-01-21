647f6a5c3dec627be33dfcaea3835d26bd0af249
liencard <lien.cardoen@student.howest.be>
2021-01-21 21:00:39 +0100

    developing header homepage #21

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,12 @@
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
-import Head from 'next/head';
-// import styles from '../styles/Home.module.css';
-import Authentication from '../components/Authentication';
-import LoginForm from '../components/Authentication/LoginForm';
-
-import Logout from '../components/Logout/Logout';
-import ProjectList from '../components/ProjectList/ProjectList';
-import ProjectCard from '../components/ProjectCard/ProjectCard';
-import Header from '../components/Header/Header';
-import { Container } from '../components/Layout';
 import { useStores } from '../hooks/useStores';
 import { ROUTES } from '../consts/index';
+
+import ProjectCard from '../components/ProjectCard/ProjectCard';
+import Header from '../components/Header/Header';
+import HeroHome from '../components/HeroHome/HeroHome';
+import { Container } from '../components/Layout';
 
 const Home = () => {
   const { uiStore } = useStores();
@@ -25,14 +20,8 @@ const Home = () => {
 
   return (
     <>
-      {/* <Authentication /> */}
       <Header />
-      {/* <p>Dag, user</p>
-      <div style={{ padding: '5px' }}>
-        <h3>My Todo Items</h3>
-        <ProjectList />
-      </div>
-      <Logout /> */}
+      <HeroHome />
 
       <Container>
         <ProjectCard />
