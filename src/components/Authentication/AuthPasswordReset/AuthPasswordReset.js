c801e38c8e692b4e385d6e1ee3bc54a18a99e92f
liencard <lien.cardoen@student.howest.be>
2021-01-24 16:30:49 +0100

    added password reset feature #45

--- src/components/Authentication/AuthPasswordReset/AuthPasswordReset.js
+++ src/components/Authentication/AuthPasswordReset/AuthPasswordReset.js
@@ -3,6 +3,7 @@ import styles from './AuthPasswordReset.module.scss';
 import TextField from '@material-ui/core/TextField';
 import { Modal, Button } from '../../UI';
 import firebase from 'firebase/app';
+import { Grid } from '../../Layout';
 
 const PasswordReset = () => {
   const [open, setOpen] = useState(false);
@@ -33,24 +34,28 @@ const PasswordReset = () => {
   return (
     <>
       <Modal open={open} handleClose={handleClose}>
-        <h2 className={styles.title} id="transition-modal-title">
-          Wachtwoord vergeten
-        </h2>
-        <p>
-          Weet je het wachtwoord niet meer? Vul hieronder je e-mailadres in. We
-          sturen dan binnen enkele minuten een e-mail waarmee een nieuw
-          wachtwoord kan worden aangemaakt.
-        </p>
-        <TextField
-          className={styles.textfield}
-          fullWidth
-          id="outlined-basic"
-          label="Email"
-          variant="outlined"
-          value={email}
-          onChange={(e) => setEmail(e.currentTarget.value)}
-        />
-        <Button text="Verzenden" onClick={handleResetPassword} />
+        <Grid>
+          <div className={styles.wrapper}>
+            <h2 className={styles.title} id="transition-modal-title">
+              Wachtwoord vergeten
+            </h2>
+            <p>
+              Weet je het wachtwoord niet meer? Vul hieronder je e-mailadres in.
+              We sturen dan binnen enkele minuten een e-mail waarmee een nieuw
+              wachtwoord kan worden aangemaakt.
+            </p>
+            <TextField
+              className={styles.textfield}
+              fullWidth
+              id="outlined-basic"
+              label="Email"
+              variant="outlined"
+              value={email}
+              onChange={(e) => setEmail(e.currentTarget.value)}
+            />
+            <Button text="Verzenden" onClick={handleResetPassword} />
+          </div>
+        </Grid>
       </Modal>
       <button className={styles.forgot} onClick={handleOpen}>
         Wachtwoord vergeten?
