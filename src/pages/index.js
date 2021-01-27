4fbc1ff445e96410f0c1207adc1ee0f491afa977
Sarah Van Den Berghe <sarah.van.den.berghe@student.howest.be>
2021-01-27 19:05:05 +0100

    test deploy

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import HomeSpotlight from '../components/Home/HomeSpotlight/HomeSpotlight';
 import ProjectSpotlight from '../components/Home/ProjectSpotlight/ProjectSpotlight';
 import { Container } from '../components/Layout';
 
+/* Test deploy */
 const Home = () => {
   return (
     <>
