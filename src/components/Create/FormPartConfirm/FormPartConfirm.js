8050085f909c8ef07b0637012a7e269d62c8eb0a
Sarah Van Den Berghe <sarah.van.den.berghe@student.howest.be>
2021-02-07 20:48:18 +0100

    form confirmation

--- src/components/Create/FormPartConfirm/FormPartConfirm.js
+++ src/components/Create/FormPartConfirm/FormPartConfirm.js
@@ -1,8 +1,15 @@
+import Confetti from 'react-confetti';
 import styles from './FormPartConfirm.module.scss';
+import useWindowSize from '@rooks/use-window-size';
+import { Button } from '../../UI';
+import { ROUTES } from '../../../consts';
 
 const FormPartConfirm = () => {
+  const { innerWidth, innerHeight } = useWindowSize();
+
   return (
     <>
+      <Confetti width={innerWidth} height={innerHeight} />
       <h2 className={styles.title}>Bedankt voor je inzending</h2>
       <p className={styles.info}>
         Alvorens je project live op de website komt, zal deze gerievewed worden door de DURF 2030. Zodra je project werd
@@ -12,6 +19,7 @@ const FormPartConfirm = () => {
       <p className={styles.info}>
         Benieuwd naar andere projecten? Bekijk projecten van andere durvers die al live staan!
       </p>
+      <Button text="Bekijk alle projecten" href={ROUTES.projects} />
     </>
   );
 };
