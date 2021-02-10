4b9d8934315e5ed5cdcef912ba5a9abb90b6605d
liencard <lien.cardoen@student.howest.be>
2021-02-11 00:34:54 +0100

    filter requirements

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -25,3 +25,4 @@ export { default as ProjectCircle } from './ProjectCircle/ProjectCircle';
 export { default as ProjectEditBanner } from './ProjectEditBanner/ProjectEditBanner';
 export { default as ProjectUpdates } from './ProjectUpdates/ProjectUpdates';
 export { default as ProjectFilter } from './ProjectFilter/ProjectFilter';
+export { default as RequirementFilter } from './RequirementFilter/RequirementFilter';
