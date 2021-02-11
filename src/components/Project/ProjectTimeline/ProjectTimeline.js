884a6be0b0ff7872c1b09a994a194ed82f8c8851
Sarah Van Den Berghe <sarah.van.den.berghe@student.howest.be>
2021-02-11 13:47:20 +0100

    detail page edits

--- src/components/Project/ProjectTimeline/ProjectTimeline.js
+++ src/components/Project/ProjectTimeline/ProjectTimeline.js
@@ -2,16 +2,17 @@ import styles from './ProjectTimeline.module.scss';
 import LinearProgress from '@material-ui/core/LinearProgress';
 
 const ProjectTimeline = ({ state }) => {
+  console.log(state);
   return (
     <div className={styles.timeline}>
       <div className={styles.progressbar}>
-        <LinearProgress variant="determinate" value={state * 33.33} />
+        <LinearProgress variant="determinate" value={state * 33.33 - 33.33} />
       </div>
       <ul className={styles.text}>
         <li className={styles.done}>Project is opgezet</li>
-        <li className={state > 0 && styles.done}>Crowdfunding is mogelijk</li>
-        <li className={state > 1 && styles.done}>Klaar om te starten</li>
-        <li className={state > 2 && styles.done}>&#127937;</li>
+        <li className={state > 1 && styles.done}>Crowdfunding is mogelijk</li>
+        <li className={state > 2 && styles.done}>Klaar om te starten</li>
+        <li className={state > 3 && styles.done}>&#127937;</li>
       </ul>
     </div>
   );
