d22c9995e71e91489911a45d91a9c2e0670bb2b3
Sarah Van Den Berghe <sarah.van.den.berghe@student.howest.be>
2021-02-11 17:06:45 +0100

    fixed error

--- src/components/Project/ProjectDurvers/ProjectDurvers.js
+++ src/components/Project/ProjectDurvers/ProjectDurvers.js
@@ -19,57 +19,19 @@ const ProjectDurvers = observer(({ project }) => {
               <div className={styles.user}>
                 <img className={styles.image} src={durver.user.avatar} alt="profielfoto van organisator" />
                 <div className={styles.wrapper}>
-              <span>
-                <p>{durver.user.name}</p>
-                {durver.fundingOffered && (
-                  <img
-                    className={styles.icon}
-                    src="/icons/money-yellow.svg"
-                    alt="icon"
-                    width="18"
-                    height="18"
-                  />
-                )}
-                {durver.materialsOffered && (
-                  <img
-                    className={styles.icon}
-                    src="/icons/mmmaterial-red.svg"
-                    alt="icon"
-                    width="15"
-                    height="15"
-                  />
-                )}
-                {durver.servicesOffered && (
-                  <img
-                    className={styles.icon}
-                    src="/icons/service-green.svg"
-                    alt="icon"
-                    width="18"
-                    height="18"
-                  />
-                )}
-              </span>
-              <p className={styles.offer}>
-                {durver.fundingOffered && (
-                  <span>
-                    {durver.fundingAmount} EUR
-                    {durver.offers.length != 0 && ', '}
-                  </span>
-                )}
-                {durver.offers.map((offer, i) => (
                   <span>
                     <p>{durver.user.name}</p>
                     {durver.fundingOffered && (
                       <img className={styles.icon} src="/icons/money-yellow.svg" alt="icon" width="18" height="18" />
                     )}
                     {durver.materialsOffered && (
-                      <img className={styles.icon} src="/icons/mmmaterial-red.svg" alt="icon" width="15" height="15" />
+                      <img className={styles.icon} src="/icons/material-red.svg" alt="icon" width="15" height="15" />
                     )}
                     {durver.servicesOffered && (
                       <img className={styles.icon} src="/icons/service-green.svg" alt="icon" width="18" height="18" />
                     )}
                   </span>
-                  <p className={styles.offer}>
+                  <div className={styles.offer}>
                     {durver.fundingOffered && (
                       <span>
                         {durver.fundingAmount} EUR
@@ -82,7 +44,7 @@ const ProjectDurvers = observer(({ project }) => {
                         {durver.offers.length !== i && ', '}
                       </span>
                     ))}
-                  </p>
+                  </div>
                 </div>
               </div>
               <p>{durver.message}</p>
